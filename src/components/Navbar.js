8488c078bd1ab392a00a296014f325205739d14d
Greshan Warnakulasuriya <greshan@ninodigitalhouse.com>
2022-11-02 15:45:34 +0530

    navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,8 @@ export default function navbar() {
           <Nav className="me-auto">
             <Nav.Link href="/">Dashboard</Nav.Link>
             <Nav.Link href="/pending-requisitions">Pending Requisitions</Nav.Link>
-            <Nav.Link href="#Vendors">Vendors</Nav.Link>
-            <Nav.Link href="#Reports">Reports</Nav.Link>
+            <Nav.Link href="">Vendors</Nav.Link>
+            <Nav.Link href="">Reports</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
